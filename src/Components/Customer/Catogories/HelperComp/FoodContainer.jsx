bc52257007bd36d015e7efd6005dfb6586a5b11b
anuragsah401 <anuragsah401@gmail.com>
2025-11-26 20:56:23 +0100

    fixed some responsive style

--- src/Components/Customer/Catogories/HelperComp/FoodContainer.jsx
+++ src/Components/Customer/Catogories/HelperComp/FoodContainer.jsx
@@ -20,14 +20,14 @@ const FoodContainer = ({ title, category }) => {
   const filteredFoodItems = foodItems?.filter((item) => item?.category === category);
 
   return (
-    <div className="container mt-[13em] md:mt-[12em] flex flex-col h-[calc(100vh-15em)]">
-      {/* Sticky Title */}
-      <div className="text-center z-50 py-2">
+    <div className="container pt-[14rem] flex flex-col h-screen">
+      {/* Sticky Title Right Under Navbar */}
+      <div className=" top-[20rem] bg-white z-40 py-2 text-center shadow-sm">
         <Title text={title} size="1.3rem" />
       </div>
 
       {/* Scrollable GRID */}
-      <div className="flex-1 overflow-y-auto mt-4 md:pt-5 bg-[#f9f9f9] rounded-lg shadow-lg p-4">
+      <div className="flex-1 overflow-y-auto mt-4 pb-6">
         {loading ? (
           <div className="flex justify-center py-8">
             <LoadingIcon />
@@ -35,7 +35,7 @@ const FoodContainer = ({ title, category }) => {
         ) : error ? (
           <div className="text-center text-red-600 mt-9 font-semibold">{error}</div>
         ) : filteredFoodItems?.length > 0 ? (
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-12 lg:gap-6 xl:gap-8 ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 px-2">
             {filteredFoodItems.map((food, i) => (
               <FoodCard food={food} key={food._id || i} />
             ))}
