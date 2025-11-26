4762d401775ee90f21d5da447d7d75b7bda5d078
anuragsah401 <anuragsah401@gmail.com>
2025-11-26 20:59:52 +0100

    fixed some responsive style

--- src/Components/Customer/Catogories/HelperComp/FoodContainer.jsx
+++ src/Components/Customer/Catogories/HelperComp/FoodContainer.jsx
@@ -20,7 +20,7 @@ const FoodContainer = ({ title, category }) => {
   const filteredFoodItems = foodItems?.filter((item) => item?.category === category);
 
   return (
-    <div className="container pt-[14rem] flex flex-col h-screen">
+    <div className="container pt-[14rem] flex flex-col h-screen fixed left-0 right-0">
       {/* Sticky Title Right Under Navbar */}
       <div className=" top-[20rem] bg-white z-40 py-2 text-center shadow-sm">
         <Title text={title} size="1.3rem" />
